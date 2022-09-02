d7342d7d82385dbf29d13a382293400a5a281d9f
Antoni <antonstrasz@gmail.com>
2022-09-02 23:20:25 +0200

    basic setup done (problem - can't get 200 return code, need to solve this)

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-print("Hello, World!")
+html = requests.get("https://bscscan.com/address/0xb667c499b88ac66899e54e27ad830d423d9fba69#tokentxns").text
+soup = BeautifulSoup(html, 'lxml')
+print(soup)
